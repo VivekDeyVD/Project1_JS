ef348768282ebf7b78d3c68a77466b9512417475
VivekDeyVD <vivekvd35@gmail.com>
2024-03-22 21:52:20 +0530

    added more information

--- script.js
+++ script.js
@@ -13,8 +13,90 @@ const generateBtn = document.querySelector(".generateButton");
 const allCheckBox = document.querySelectorAll("input[type=checkbox]");
 const symbols = '~`!@#$%^&*()_-+={[}]|:;"<,>.?/';
 
-let password="";
-let passwordLength=10;
-let checkCount=1;
 
-//set strength circle to grey
+//initially
+let password = "";
+let passwordLength = 10;
+let checkCount = 0;
+handleSlider();
+//ste strength circle color to grey
+setIndicator("#ccc");
+
+
+//set passwordLength
+function handleSlider() {
+    inputSlider.value = passwordLength;
+    lengthDisplay.innerText = passwordLength;
+    //or kuch bhi karna chahiye ? - HW
+}
+
+//set indicator (colour)
+
+function setIndicator(){
+    indicator.style.backgroundColor=color;
+    //shadow
+}
+
+function getRndInteger (min,max) {
+    return Math.floor(Math.random() * (max-min))+min;
+
+}
+
+function generateRandomNumber (){
+    return getRndInteger(0,9);
+}
+
+function generateLowerCase(){
+    return String.fromCharCode(getRndInteger(97,123));
+}
+
+function generateUpperCase(){
+    return String.fromCharCode(getRndInteger(65,91))
+}
+
+function generateSymbol(){
+    const randNum = getRndInteger(0,symbols.length);
+    return symbols.charAt(randNum);
+}
+
+function calcStrength(){
+
+ let hasUpper = false;
+    let hasLower = false;
+    let hasNum = false;
+    let hasSym = false;
+
+    //.checked is for checking whether the checkbox is checked or not
+    if (uppercaseCheck.checked) hasUpper = true;
+    if (lowercaseCheck.checked) hasLower = true;
+    if (numbersCheck.checked) hasNum = true;
+    if (symbolsCheck.checked) hasSym = true;
+  
+    if (hasUpper && hasLower && (hasNum || hasSym) && passwordLength >= 8) {
+      setIndicator("#0f0");
+    } else if (
+      (hasLower || hasUpper) &&
+      (hasNum || hasSym) &&
+      passwordLength >= 6
+    ) {
+      setIndicator("#ff0");
+    } else {
+      setIndicator("#f00");
+    }
+}
+
+async function copyContent (){
+    try{
+        await  navigator.clipboard.writeText(passwordDisplay.value );
+        copyMsg.innerText="Copied";
+    }
+    catch(e){
+        copyMsg.innerText="Failed";
+
+    }
+    //to make copy span visible
+    copyMsg.classList.add("active");
+   setTimeout( () => {
+    copyMsg.classList.remove("active");
+   },2000);
+}
