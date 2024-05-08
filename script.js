2b94d02c587af047af7e30449307d46033e60583
Vivek  Dey <146002603+VivekDeyVD@users.noreply.github.com>
2024-05-08 16:52:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const allCheckBox = document.querySelectorAll("input[type=checkbox]");
 const symbols = '~`!@#$%^&*()_-+={[}]|:;"<,>.?/';
 
 
-//initially
+//initiLLY..
 let password = "";
 let passwordLength = 10;
 let checkCount = 0;
